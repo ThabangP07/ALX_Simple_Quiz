d29434e620f13434e9543d2f82c841e17aa62941
Thabang <phoshokothabang1@gmail.com>
2025-08-03 08:51:29 +0200

    feat: access the calculation operator buttons

--- calculator.js
+++ calculator.js
@@ -14,3 +14,7 @@ function divide(number1, number2) {
     return number1 / number2;
 }
 
+const addition = document.getElementById('add');
+const subtraction = document.getElementById('subtract');
+const multiplication = document.getElementById('multiply');
+const division = document.getElementById('divide');
