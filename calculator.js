1f5212ee417216bd1ccf6ca887760db31e1a135e
Thabang <phoshokothabang1@gmail.com>
2025-08-03 08:57:12 +0200

    feat: access the input values and set result to zero in th global scope

--- calculator.js
+++ calculator.js
@@ -18,3 +18,9 @@ const addition = document.getElementById('add');
 const subtraction = document.getElementById('subtract');
 const multiplication = document.getElementById('multiply');
 const division = document.getElementById('divide');
+
+const num1 = parseFloat(document.getElementById('number1').value) || 0;
+const num2 = parseFloat(document.getElementById('number2').value) || 0;
+let result = 0;
+
+
